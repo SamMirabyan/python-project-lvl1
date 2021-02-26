8abe57ae54d8f78b4cc677a82eeea14fa038c401
SamMirabyan <samvel.mirabyan@gmail.com>
2021-02-26 23:01:33 +0300

    add cli.py; modify brain_games.py; modify poetry.lock and pyproject.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+import os
+import os.path
+import sys
+
+sys.path.append(os.getcwd() + "\\brain_games")
+from brain_games import cli
+
 def main():
     print("Welcome to the Brain Games!")
+    cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
