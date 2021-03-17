a6760d6e1439ec55c92da515119495348ec0260c
SamMirabyan <samvel.mirabyan@gmail.com>
2021-03-17 21:40:23 +0300

    simplifed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
+import prompt
 
 
 def main():
     print("Welcome to the Brain Games!")
-    user_name = welcome_user()
-    return user_name
+    user_name = prompt.string('May I have your name? ')
+    print(f"Hello, {user_name.title()}!")
 
 
 if __name__ == '__main__':
